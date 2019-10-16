d9d9708e9577488102fda44057c789e4b169a578
Gomozov <valeriy.gomozov@gmail.com>
2019-10-16 21:30:52 +0300

    add parenthesis

--- calc.js
+++ calc.js
@@ -1,28 +1,55 @@
-const Calculator = function() {
-  this.evaluate = str => {
-    let aux;
-    str = str.replace(/\s/g,"");
-    if (/[^0-9\+-\/\*eE\.]/.test(str)) {
-      return "Please enter digits and math operators only.";
-    }
-    while (/\d[\*|\/]-?\d+/.test(str)) {
-      aux = /(-?\d*\.?\d+(?:[eE][-+]\d+)?)(\*|\/)(-?\d*\.?\d+(?:[eE][-+]\d+)?)/.exec(str);
-      if (aux[2] === "*") { 
-        str = str.replace(/-?\d*\.?\d+(?:[eE][-+]\d+)?\*-?\d*\.?\d+(?:[eE][-+]\d+)?/, aux[1]*aux[3]);
-      } else {
-        str = str.replace(/-?\d*\.?\d+(?:[eE][-+]\d+)?\/-?\d*\.?\d+(?:[eE][-+]\d+)?/, aux[1]/aux[3]);
-      }
-    }
-    while (/\d[\+|-]-?\d/.test(str)) {
-      aux = /(-?\d*\.?\d+(?:[eE][-+]\d+)?)(\+|-)(-?\d*\.?\d+(?:[eE][-+]\d+)?)/.exec(str);
-      if (aux[2] === "+") { 
-        str = str.replace(/-?\d*\.?\d+(?:[eE][-+]\d+)?\+-?\d*\.?\d+(?:[eE][-+]\d+)?/, Number(aux[1])+Number(aux[3]));
-      } else {
-        str = str.replace(/-?\d*\.?\d+(?:[eE][-+]\d+)?--?\d*\.?\d+(?:[eE][-+]\d+)?/, Number(aux[1])-Number(aux[3]));
-      }
-    }
-    return str;
+let calc = function (str) {
+  str = str.replace(/\s/g,"");
+  if (/[^0-9\+-\/\*eE\.]/.test(str)) {
+    return "Please enter digits and math operators only.";
   }
+  str = calculate_parenthesis(str);  
+  str = calculate_multiplicator(str);
+  str = calculate_addictive(str);
+  return Number(str);
+};
+
+let calculate_parenthesis = function (str) {
+  let aux = /(?:[^\d](-))?\(([^\(\)]*)\)/.exec(str);
+  let aux1="";
+  while (aux) {
+    aux1 = calculate_multiplicator(aux[2]);
+    if (aux[1]) {
+      aux1 = (-1)*calculate_addictive(aux1);
+      str = str.replace(aux[0].substr(1), aux1);
+    } else {
+      aux1 = calculate_addictive(aux1);
+      str = str.replace(aux[0], aux1);
+    } 
+    aux = /(?:[^\d](-))?\(([^\(\)]*)\)/.exec(str);
+  }
+  return str;
+};
+
+let calculate_addictive = function (str) {  
+  let aux = /(-?\d*\.?\d+(?:e[-+]\d+)?)(\+|-)(-?\d*\.?\d+(?:e[-+]\d+)?)/.exec(str);
+  while (aux) {
+    if (aux[2] === "+") { 
+      str = str.replace(/-?\d*\.?\d+(?:e[-+]\d+)?\+-?\d*\.?\d+(?:e[-+]\d+)?/, Number(aux[1]) + Number(aux[3]));
+    } else {
+      str = str.replace(/-?\d*\.?\d+(?:e[-+]\d+)?--?\d*\.?\d+(?:e[-+]\d+)?/, Number(aux[1]) - Number(aux[3]));
+    }
+    aux = /(-?\d*\.?\d+(?:e[-+]\d+)?)(\+|-)(-?\d*\.?\d+(?:e[-+]\d+)?)/.exec(str);
+  }
+  return str;
+};
+
+let calculate_multiplicator = function (str) {
+  let aux = /(-?\d*\.?\d+(?:e[-+]\d+)?)(\*|\/)(-?\d*\.?\d+(?:e[-+]\d+)?)/.exec(str);
+  while (aux) {
+    if (aux[2] === "*") { 
+      str = str.replace(/-?\d*\.?\d+(?:e[-+]\d+)?\*-?\d*\.?\d+(?:e[-+]\d+)?/, aux[1] * aux[3]);
+    } else {
+      str = str.replace(/-?\d*\.?\d+(?:e[-+]\d+)?\/-?\d*\.?\d+(?:e[-+]\d+)?/, aux[1] / aux[3]);
+    }
+    aux = /(-?\d*\.?\d+(?:e[-+]\d+)?)(\*|\/)(-?\d*\.?\d+(?:e[-+]\d+)?)/.exec(str);
+  }
+  return str;
 };
 
 const rl = require('readline').createInterface({
@@ -30,13 +57,12 @@ const rl = require('readline').createInterface({
   output: process.stdout,
   prompt: 'CALCULATOR> '
 })
-let calc = new Calculator();
 
 rl.prompt();
 rl.on('line', (line) => {
   switch (line.trim()) {
     default:
-      console.log(calc.evaluate(line.trim()));
+      console.log(calc(line.trim()));
       break;
   }
   rl.prompt();
